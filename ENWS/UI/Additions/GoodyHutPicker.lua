4c3d8bb0336afe1338403bfb4c011bfcac74581a
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2023-11-16 10:11:03 -0800

    v6 Update
    - Updated MainMenu.xml to be compatible with the November 2023 game update
    - Added dummy lua file for the Goody Hut Picker (EGHV) to prevent log errors when that mod is not present

---
+++ ENWS/UI/Additions/GoodyHutPicker.lua
@@ -0,0 +1,14 @@
+--[[ =========================================================================
+	ENWS : Enhanced Natural Wonders Selection for Civilization VI
+	Copyright (C) 2020-2023 zzragnar0kzz
+	All rights reserved
+=========================================================================== ]]
+
+--[[ =========================================================================
+	begin GoodyHutPicker.lua frontend script
+=========================================================================== ]]
+
+--[[ =========================================================================
+	this is a dummy file to prevent errors in the lua log from this file not being present
+    when EGHV is present and enabled, this file will be overwritten by the actual GoodyHutPicker script
+=========================================================================== ]]
